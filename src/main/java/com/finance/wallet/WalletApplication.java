e947e9ee07111f5e5b704954a38362a31dc1a2b8
srinivasareddykovvuri116-dot <srinivasareddykovvuri116@gmail.com>
2026-09-12 13:43:26 +0530

    Clean up Spring Data page serialization

--- src/main/java/com/finance/wallet/WalletApplication.java
+++ src/main/java/com/finance/wallet/WalletApplication.java
@@ -2,12 +2,14 @@ package com.finance.wallet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
 public class WalletApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WalletApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(WalletApplication.class, args);
+    }
 }
